5d3e1eb1d97ccfb25d27914f85225af7da71732b
Harshil <darjiharshil2994@gmail.com>
2021-02-12 09:27:15 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="location-cryptography", #Feel free to change this! 
+    name="location-cryptography",
     version="1.0.0",
-    author="Author", #Fill this out
-    author_email="author@example.com", #fill this out
-    description="A small example package", #fill this out
+    author="Harshil Darji",
+    author_email="darjiharshil2994@gmail.com",
+    description="Location based Cryptography uses location of the device in addition to the pass-phrase as key.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/harshildarji/location_based_cryptography",
